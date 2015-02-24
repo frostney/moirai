89cae65cae5292eddd4dc8dd0e198d9f25d8c7c1
Johannes Stein <johannesstein@freeze-dev.com>
2015-02-24 23:53:09 +0100

    Update gameobject

--- src/gameobject.js
+++ src/gameobject.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
 var GameObject = React.createClass({
+  getInitialProps: function() {
+    return {
+      position: '0,0'
+    }
+  },
   render: function() {
+    var position = 'absolute';
+    
+    var pos = this.props.position;
+    if (typeof pos === 'string') {
+      pos = pos.split(',');
+    }
+    
+    var left = pos[0] + 'px';
+    var top = pos[1] + 'px';
+    
+    var style = {position, left, top};
     
     return (
-      <div className="gameobject">
+      <div className="gameobject" style={style}>
 				{this.props.children}
       </div>
     );
