3f2693f754f02b68f4ab33231726b9ddde960221
Johannes Stein <johannesstein@freeze-dev.com>
2015-02-25 20:49:24 +0100

    Update viewport

--- src/viewport.js
+++ src/viewport.js
@@ -10,8 +10,9 @@ var Viewport = React.createClass({
   render: function() {
     var width = (this.props.width === 'auto') ? '100%' : this.props.width + 'px';
     var height = (this.props.height === 'auto') ? '100%' : this.props.height + 'px';
-
-    var style = {width, height};
+    var transform = 'scale(1.0, 1.0)';
+    
+    var style = {width, height, transform};
     
     return (
       <div className="viewport" style={style}>
@@ -20,5 +21,26 @@ var Viewport = React.createClass({
     );
   }
 });
+
+/*
+ var WindowDimensions = React.createClass({
+ render: function() {
+ return <span>{this.state.width} x {this.state.height}</span>;
+ },
+ updateDimensions: function() {
+ this.setState({width: $(window).width(), height: $(window).height()});
+ },
+ componentWillMount: function() {
+ this.updateDimensions();
+ },
+ componentDidMount: function() {
+ window.addEventListener("resize", this.updateDimensions);
+ },
+ componentWillUnmount: function() {
+ window.removeEventListener("resize", this.updateDimensions);
+ }
+ });
+
+ */
 
 export default Viewport;
