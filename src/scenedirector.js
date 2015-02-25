f37bb5dab235b57472a0e42f4f71e3a519c0a89e
Johannes Stein <johannesstein@freeze-dev.com>
2015-02-25 20:49:16 +0100

    Update scene director

--- src/scenedirector.js
+++ src/scenedirector.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 var SceneDirector = React.createClass({
   render: function() {
+    var style = {
+      width: '100%',
+      height: '100%'
+    };
+    
     return (
-      <div className="scene-director">
+      <div className="scene-director" style={style}>
         {this.props.children}
       </div>
     );
