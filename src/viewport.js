6da79d637c944543d19b72c3784182e0a6db29fc
Johannes Stein <johannesstein@freeze-dev.com>
2015-02-25 21:36:24 +0100

    Viewport logic

--- src/viewport.js
+++ src/viewport.js
@@ -1,13 +1,38 @@
 import React from 'react';
 
 class Viewport extends React.Component {
+  constructor() {
+    this.state = {
+      windowWidth: 0,
+      windowHeight: 0
+    };
+
+    this.updateDimensions = this.updateDimensions.bind(this);
+  }
+
+  updateDimensions() {
+    this.setState({width: window.innerWidth, height: window.innerHeight});
+  }
+
+  componentWillMount() {
+    this.updateDimensions();
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.updateDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions);
+  }
+
   render() {
     var width = (this.props.width === 'auto') ? '100%' : this.props.width + 'px';
     var height = (this.props.height === 'auto') ? '100%' : this.props.height + 'px';
     var transform = 'scale(1.0, 1.0)';
-    
+
     var style = {width, height, transform};
-    
+
     return (
       <div className="viewport" style={style}>
         {this.props.children}
@@ -21,26 +46,5 @@ Viewport.defaultProps = {
   height: 'auto',
   mode: 'fit'
 };
-
-/*
- var WindowDimensions = React.createClass({
- render: function() {
- return <span>{this.state.width} x {this.state.height}</span>;
- },
- updateDimensions: function() {
- this.setState({width: $(window).width(), height: $(window).height()});
- },
- componentWillMount: function() {
- this.updateDimensions();
- },
- componentDidMount: function() {
- window.addEventListener("resize", this.updateDimensions);
- },
- componentWillUnmount: function() {
- window.removeEventListener("resize", this.updateDimensions);
- }
- });
-
- */
 
 export default Viewport;
