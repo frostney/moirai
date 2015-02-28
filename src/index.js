2609b111114fcb99e7559981caeaed6f154bed07
Johannes Stein <johannesstein@freeze-dev.com>
2015-02-28 19:52:24 +0100

    Add index file

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Game from 'moirai/game';
+import Viewport  from 'moirai/viewport';
+import GameObject from 'moirai/gameobject';
+import Scene from 'moirai/scene';
+import SceneDirector from 'moirai/scenedirector';
+
+export default {Game, Viewport, GameObject, Scene, SceneDirector};
