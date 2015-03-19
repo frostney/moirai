30b9bd3011f5be58c01d9468735f066bf02b8587
Johannes Stein <johannesstein@freeze-dev.com>
2015-03-19 23:21:55 +0100

    Evil trick

--- src/asset.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-class Asset extends React.Component {
-  
-}
-
-export default Asset;
--- src/behavior.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Behavior extends React.Component {
-  render() {
-    return null;
-  }
-}
-
-export default Behavior;
