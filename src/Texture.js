d6c594e9b57653d26de902f0ac0cf4049dc00c2b
Johannes Stein <johannesstein@freeze-dev.com>
2015-04-16 17:43:09 +0200

    Texture can't render anything on its own

--- src/Texture.js
+++ src/Texture.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 class Texture extends React.Component {
-  
+  render() {
+    return null;
+  }
 }
+
+Texture.propTypes = {
+  x: React.PropTypes.number,
+  y: React.PropTypes.number,
+  width: React.PropTypes.oneOfType([React.PropTypes.number, React.PropTypes.string])
+  height: React.PropTypes.oneOfType([React.PropTypes.number, React.PropTypes.string])
+};
+
+Texture.defaultProps = {
+  x: 0,
+  y: 0,
+  width: 'auto',
+  height: 'auto'
+};
 
 export default Texture;
