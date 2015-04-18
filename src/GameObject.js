cdc477712e4e19194f6bdd1648131a518171af93
Johannes Stein <johannesstein@freeze-dev.com>
2015-04-18 15:13:37 +0200

    x and y are now on the state

--- src/GameObject.js
+++ src/GameObject.js
@@ -1,25 +1,45 @@
 import React from 'react';
 
 class GameObject extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      x: props.initialX,
+      y: props.initialY
+    };
+  }
+  processChildren(children) {
+    
+  }
   render() {
     var position = 'absolute';
-    
-    var left = this.props.x;
-    var top = this.props.y;
-    
+
+    var left = this.state.x;
+    var top = this.state.y;
+
     var style = {position, left, top};
-    
+
     return (
       <div className="gameobject" style={style}>
-				{this.props.children}
+        {this.props.children}
       </div>
     );
   }
 }
 
+GameObject.propTypes = {
+  initialX: React.PropTypes.number,
+  initialY: React.PropTypes.number,
+  width: React.PropTypes.oneOfType([React.PropTypes.number, React.PropTypes.string]),
+  height: React.PropTypes.oneOfType([React.PropTypes.number, React.PropTypes.string])
+};
+
 GameObject.defaultProps = {
-  x: 0,
-  y: 0
+  initialX: 0,
+  initialY: 0,
+  width: 'auto',
+  height: 'auto'
 };
 
 export default GameObject;
