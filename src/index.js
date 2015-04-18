cc927e29e981823c6c955742b77158916891b24e
Johannes Stein <johannesstein@freeze-dev.com>
2015-04-18 17:24:05 +0200

    Update exports

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import SceneDirector from './SceneDirector';
 import Audio from './Audio';
 import Texture from './Texture';
 import Behavior from './Behavior'
+import create from './create';
 
-export default {Viewport, GameObject, Scene, SceneDirector, Audio, Texture, Behavior};
+export default {create, Viewport, GameObject, Scene, SceneDirector, Audio, Texture, Behavior};
