8ce1dfba3090f186ee7f2cb7bf4b10476f3afc1d
Johannes Stein <johannesstein@freeze-dev.com>
2015-04-19 00:03:18 +0200

    Optional parenthesis are optional

--- src/create.js
+++ src/create.js
@@ -17,7 +17,7 @@ const create = function(gameContent) {
       }
 
       // Bind the game loop to the `update` event
-      Loop.on('update', (dt) => {
+      Loop.on('update', dt => {
         // Deltatime should not be a millisecond value, but a second one.
         // It should be a value between 0 - 1
         Events.trigger('update', dt / 1000);
