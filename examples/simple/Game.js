95eaad3d2d3b84f779b51b2c521ec5d5bbdc8bf7
Johannes Stein <johannesstein@freeze-dev.com>
2015-04-18 17:36:46 +0200

    Update example configuration

--- examples/simple/Game.js
+++ examples/simple/Game.js
@@ -1,1 +1,17 @@
-import 
+import React from 'react';
+import Moirai from 'moirai';
+
+const {create, Viewport, SceneDirector, Scene, GameObject, Texture} = Moirai;
+
+create(
+  <Viewport>
+    <SceneDirector>
+      <Scene>
+        <GameObject>
+          <Texture source="something.png" />
+          <Behavior onUpdate={function() {}} />
+        </GameObject>
+      </Scene>
+    </SceneDirector>
+  </Viewport>
+).render();
