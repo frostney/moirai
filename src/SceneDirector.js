6c9c5d2db3d4f4105c0a7e52d4863b4d71aeb244
Johannes Stein <johannesstein@freeze-dev.com>
2015-05-28 08:57:30 +0200

    Preparing scene management

--- src/SceneDirector.js
+++ src/SceneDirector.js
@@ -1,15 +1,31 @@
 import React from 'react';
 
+import Scene from './Scene';
+
 class SceneDirector extends React.Component {
   render() {
     var style = {
       width: '100%',
       height: '100%'
     };
-    
+
+    var currentScene = null;
+
+    if (this.props.currentScene) {
+      React.Children.forEach(this.props.children, child => {
+        if (child.type === Scene) {
+          if (child.props.name === this.props.currentScene) {
+            currentScene = child;
+          }
+        }
+      });
+    } else {
+      currentScene = this.props.children;
+    }
+
     return (
       <div className="scene-director" style={style}>
-        {this.props.children}
+        {currentScene}
       </div>
     );
   }
