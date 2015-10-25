43c3e894ba4db78e010e1ed14ebe19bdd6f8928f
Johannes Stein <johannesstein@freeze-dev.com>
2015-10-25 14:59:58 +0100

    Loop now happens in SceneDirector

--- src/SceneDirector.js
+++ src/SceneDirector.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 import Scene from './Scene';
 
+import {Loop} from 'gamebox';
+import Events from './Events';
+
 class SceneDirector extends React.Component {
+  componentDidMount() {
+    // Bind the game loop to the `update` event
+    Loop.on('update', dt => {
+      // Deltatime should not be a millisecond value, but a second one.
+      // It should be a value between 0 - 1
+      Events.trigger('update', dt / 1000);
+    });
+
+    Loop.run();
+  }
+
+  componentWillUmount() {
+    // TODO: Well, stop these fancy events!
+  }
+
   render() {
     var style = {
       width: '100%',
