1891f01f829a807eba3fe0f8811377754b2bf2da
Johannes Stein <johannesstein@freeze-dev.com>
2016-04-02 18:06:25 +0100

    Use export extensions

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
-import Viewport  from './Viewport';
-import GameObject from './GameObject';
-import Scene from './Scene';
-import SceneDirector from './SceneDirector';
-import Audio from './Audio';
-import Texture from './Texture';
-import Behavior from './Behavior';
-import Events from './Events';
-
-let Moirai = {};
-
-Moirai.Viewport = Viewport;
-Moirai.GameObject = GameObject;
-Moirai.Scene = Scene;
-Moirai.SceneDirector = SceneDirector;
-Moirai.Audio = Audio;
-Moirai.Events = Events;
-Moirai.Texture = Texture;
-Moirai.Behavior = Behavior;
-
-export default Moirai;
+export Viewport  from './Viewport';
+export GameObject from './GameObject';
+export SceneDirector from './SceneDirector';
+export Audio from './Audio';
+export Texture from './Texture';
+export Behavior from './Behavior';
+export Events from './Events';
