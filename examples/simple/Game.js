c7fe3729226bc2ab1a2b274d284bf9a8b112ec4a
Troy Goode <troygoode@gmail.com>
2016-04-20 15:02:42 -0700

    resolve issue #10 (#11)
    * get /examples to build by only using rollup-babel-lib-bundler during npm run build
    
    * fix incorrect usage of SceneDirector

--- examples/simple/Game.js
+++ examples/simple/Game.js
@@ -1,21 +1,21 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-import {Viewport, SceneDirector, Scene, GameObject, Behavior, Texture} from 'moirai';
+import {Viewport, SceneDirector, GameObject, Behavior, Texture} from 'moirai';
+
+const Scene1 = () => (
+  <GameObject>
+    <Texture source="http://facebook.github.io/react/img/logo.svg"/>
+    <Behavior
+      onUpdate={function(dt, gameObject) { gameObject.setState({x: gameObject.state.x + (dt * 10)}); }}/>
+  </GameObject>
+)
 
 class Game extends Component {
   render() {
     return (
       <Viewport>
-        <SceneDirector>
-          <Scene>
-            <GameObject>
-              <Texture source="http://facebook.github.io/react/img/logo.svg"/>
-              <Behavior
-                onUpdate={function(dt, gameObject) { gameObject.setState({x: gameObject.state.x + (dt * 10)}); }}/>
-            </GameObject>
-          </Scene>
-        </SceneDirector>
+        <SceneDirector scenes={{Scene1}} />
       </Viewport>
     );
   }
